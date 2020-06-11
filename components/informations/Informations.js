7f2c7c5284b31621ba16001dee29f1be02b8999e
RRudi <maboitemail.romeu@gmail.com>
2020-06-11 20:04:25 +0200

    Alimentation de la liste des prières

--- components/informations/Informations.js
+++ components/informations/Informations.js
@@ -1,27 +1,100 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import moment from 'moment';
 import TempsRestant from './TempsRestant';
 import TempsPriere from './TempsPriere';
 import HeureProchainePriere from './HeureProchainePriere';
 
+class Priere {
+  Libelle;
+  Horaire;
+  Debut;
+  Fin;
+}
+
 export default class Informations extends Component {
 
-urlApi = "https://api.aladhan.com/timingsByAddress/10-06-2020?address=Paris,France&midnightmode=1&method=99&methodSettings=17,null,14&tune=0,1,0,0,-1,3,3,-2,0,0";
+  urlApi = "https://api.aladhan.com/timingsByAddress/10-06-2020?address=Paris,France&midnightmode=1&method=99&methodSettings=17,null,14&tune=0,1,0,0,-1,3,3,-2,0,0";
+  listePriereBloquee = ["Imsak", "Sunset", "Midnight"];
 
-  state = {
-    horaires: []
+  constructor(props) {
+    super(props);
+    this.state = {
+      listePriere: []
+    }
   }
+
 
   componentDidMount() {
 
-    axios.get(this.urlApi)
-      .then(reponse => {
+    axios.get(this.urlApi).then(reponse => {
 
         const horaires = reponse.data.data.timings;
-        this.setState({ horaires });
+        const listePriere = [];
+
+        for (let element in horaires) 
+        {
+          if(!this.listePriereBloquee.includes(`${element}`))
+          {
+            const libelle = `${element}`;
+            const horaire = `${horaires[element]}`;
+
+            const nouvellePriere = new Priere();
+            nouvellePriere.Libelle = libelle;
+            nouvellePriere.Horaire = horaire;
+            nouvellePriere.Debut = this.spliterHoraireEtGetTimestampUTC(horaire);
+            nouvellePriere.Fin = "";
+
+            listePriere.push(nouvellePriere);
+          }
+        }
+
+        const horaireFajr = moment(listePriere.find( x => x.Libelle === 'Fajr').TimestampUTC);
+        const horaireMaghib = moment(listePriere.find( x => x.Libelle === 'Maghrib').TimestampUTC);
+        const horaireLimiteAsr = horaireMaghib.clone().subtract(30, 'm');
+        const horaireFajrProchain = horaireFajr.clone().add(1, 'd');
+        const differenceMinutes = horaireFajrProchain.diff(horaireMaghib, 'h', true);
+        const horaireMidnight = horaireMaghib.clone().add(differenceMinutes/2, 'h');
+
+        const nouvellePriere = new Priere();
+        nouvellePriere.Libelle = "FajrProchain";
+        nouvellePriere.Horaire = horaireFajrProchain.hours() + ':' + horaireFajrProchain.minute();
+        nouvellePriere.Debut = horaireFajrProchain.format();
+        nouvellePriere.Fin = "";
+        listePriere.push(nouvellePriere);
+
+        const nouvellePriere = new Priere();
+        nouvellePriere.Libelle = "Midnight";
+        nouvellePriere.Horaire = "horaire";
+        nouvellePriere.Debut = horaireMidnight.format();
+        nouvellePriere.Fin = "";
+        listePriere.push(nouvellePriere);
+
+        const nouvellePriere = new Priere();
+        nouvellePriere.Libelle = "LimiteAsr";
+        nouvellePriere.Horaire = horaireLimiteAsr.hours() + ':' + horaireLimiteAsr.minute();
+        nouvellePriere.Debut = horaireLimiteAsr.format();
+        nouvellePriere.Fin = "";
+        listePriere.push(nouvellePriere);
+
+        listePriere.sort((a, b) => moment(a.TimestampUTC) - moment(b.TimestampUTC))
+
+        const priereActuelle = listePriere.find( x => moment(x.TimestampUTC) < moment())
+
+        this.setState({ listePriere });
+        console.log(this.state.listePriere);
+
 
       })
+  }
 
+  spliterHoraireEtGetTimestampUTC(horaire){
+
+    const horaireSplit = horaire.split(':');
+    const heure = horaireSplit[0];
+    const minutes = horaireSplit[1];
+    const nouvelleDate = moment().set({'hour': heure, 'minute': minutes});
+    return nouvelleDate.format();
   }
 
   render() {
@@ -31,14 +104,8 @@ urlApi = "https://api.aladhan.com/timingsByAddress/10-06-2020?address=Paris,Fran
         <TempsRestant />
         <HeureProchainePriere />
         
-        <ul>
-          <li>Fajr : { this.state.horaires.Fajr }</li>
-          <li>Levé du soleil : { this.state.horaires.Sunrise }</li>
-          <li>Dhor : { this.state.horaires.Dhuhr }</li>
-          <li>Asr : { this.state.horaires.Asr }</li>
-          <li>Maghrib : { this.state.horaires.Maghrib }</li>
-          <li>Icha : { this.state.horaires.Isha }</li>
-        </ul>
+        <br />
+        { this.state.heureActuelle }
       </div>
     )
   }
